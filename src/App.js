d0b5c23ff84fc4b945c05f8d8f055806b324b3aa
Rishabh Gupta <guptarishabh1122@gmail.com>
2024-10-31 10:09:25 +0530

    added nested children in the cofig file of appRouter which is a createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+import Profile from "./components/Profile";
 
 const AppLayout = () => {
   return (
@@ -35,6 +36,12 @@ const appRouter = createBrowserRouter([
       {
         path: "/about",
         element: <About />,
+        children:[
+          {
+            path:"profile",
+            element:<Profile />
+          }
+        ]
       },
       {
         path: "/contact",
--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
+import { Outlet } from "react-router-dom"
+
 const About = ()  => {
     return(
+        <>
         <h1>
             About Us Page!!
         </h1>
+        <Outlet /> {/* this outlet is being used as the profile page is the children of about page which is 
+        there in the config file in  App.j*/}
+        </>
     )
 }
 
