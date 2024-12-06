d29842fbf30fb3de01f1787445314c317f1df70d
Rishabh Gupta <guptarishabh1122@gmail.com>
2024-12-06 14:45:46 +0530

    fixed the hide button bug

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Section = ({ title, description, isVisible, setIsVisible }) => {
+const Section = ({ title, description, isVisible, toggleVisibility }) => {
 //   const [isVisible, setIsVisible] = useState(false);
   return (
     <div className="border border-black p-3 m-2">
@@ -8,14 +8,16 @@ const Section = ({ title, description, isVisible, setIsVisible }) => {
       {isVisible ? (
         <button
           className="cursor-pointer underline"
-          onClick={() => setIsVisible(false)}
+        //   onClick={() => setIsVisible(false)}
+        onClick={() => toggleVisibility()}
         >
           Hide
         </button>
       ) : (
         <button
           className="cursor-pointer underline"
-          onClick={() => setIsVisible(true)}
+        //   onClick={() => setIsVisible(true)}
+        onClick={() => toggleVisibility()}
         >
           Show
         </button>
@@ -28,6 +30,11 @@ const Section = ({ title, description, isVisible, setIsVisible }) => {
 
 const Instamart = () => {
     const [visibleSection, setVisibleSection] = useState("")
+    const toggleVisibility = (section) => {
+        setVisibleSection(visibleSection === section ? "" : section);
+    }
+
+
   return (
     <div>
       <h1 className="text-3xl font-bold m-2 p-2">INSTAMART</h1>
@@ -37,7 +44,8 @@ const Instamart = () => {
           "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains"
         }
         isVisible={visibleSection === "about"}
-        setIsVisible={() => setVisibleSection("about")}
+        // setIsVisible={() => setVisibleSection("about")}
+        toggleVisibility={() => toggleVisibility("about")}
       />
       <Section
         title={"Team"}
@@ -45,7 +53,8 @@ const Instamart = () => {
           "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains"
         }
         isVisible={visibleSection === "team"}
-        setIsVisible={() => setVisibleSection("team")}
+        // setIsVisible={() => setVisibleSection("team")}
+        toggleVisibility={() => toggleVisibility("team")}
       />
       <Section
         title={"Career"}
@@ -53,7 +62,8 @@ const Instamart = () => {
           "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains"
         }
         isVisible={visibleSection === "careers"}
-        setIsVisible={() => setVisibleSection("careers")}
+        // setIsVisible={() => setVisibleSection("careers")}
+        toggleVisibility={() => toggleVisibility("careers")}
       />
     </div>
   );
