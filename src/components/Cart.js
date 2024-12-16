c663c1a041d4ad78ee1afe03bdb84bf107465599
Rishabh Gupta <guptarishabh1122@gmail.com>
2024-12-16 11:24:57 +0530

    added clearCart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,7 @@
 import { useSelector , useDispatch} from "react-redux";
 import store from "../utils/store";
 import FoodItem from "./FoodItem";
-import { removeItem } from "../utils/cartSlice";
+import { removeItem, clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
 
@@ -12,20 +12,24 @@ const Cart = () => {
     const handleRemoveItem = (item) => {
         dispatch(removeItem(item));
     }
+    const handleClearCart = () => {
+        dispatch(clearCart());
+    }
 
   return (
 
 
     <div>
       <h1 className="font-bold text-3xl text-center">Cart Items- {cartItems.length}</h1>
+      { cartItems.length > 0 && <button className="p-3 m-3 bg-green-100" onClick={() => handleClearCart()} >Clear Cart</button>}
       <div className="flex p-2 m-3 flex-row flex-wrap">
 
       {cartItems.map((item) => {
         return (
-            <>
+            <div className="flex flex-col">
         <FoodItem key={item?.card?.info.id} {...item?.card?.info} />
         <button onClick={() => handleRemoveItem(item)} className="bg-red-400 p-2 m-5 " >Remove Item</button>
-            </>
+            </div>
         )
       }
     )}
